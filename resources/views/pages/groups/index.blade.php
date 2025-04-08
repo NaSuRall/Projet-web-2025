90ae0299cb1e74f68723be0ba3622670884fd7d1
Romain Poulain <romain.poulain@edu.esiee-it.fr>
2025-04-08 11:56:00 +0200

    Add: | Froms input dropdown to groups index

--- resources/views/pages/groups/index.blade.php
+++ resources/views/pages/groups/index.blade.php
@@ -6,4 +6,19 @@
             </span>
         </h1>
     </x-slot>
+
+
+    <form class="card-body flex flex-row gap-5 align-middle bg-gray-100" action="" method="POST">
+        <x-forms.input type="number" name="number" :label="__('Nombre de personnes dans les groupes')" placeholder="2 - 5"/>
+        <x-forms.dropdown type="select" :label="__('Promotions')"/>
+            @for
+
+        <x-forms.input type="date" name="year" :label="__('Fin de l\'année')" placeholder="" />
+
+        <x-forms.primary-button>
+            {{ __('Valider') }}
+        </x-forms.primary-button>
+
+    </form>
+
 </x-app-layout>
